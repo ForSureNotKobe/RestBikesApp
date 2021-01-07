58730e23004f8a354a39b43e52bf5aaf2c22ab91
Jakub Porębski <j.porebski0@gmail.com>
2021-01-07 13:46:52 +0100

    Minor chat changes, added telemetry

--- CourseRESTApp/Startup.cs
+++ CourseRESTApp/Startup.cs
@@ -31,6 +31,8 @@ namespace CourseRESTApp
 
             services.AddDbContext<BikesContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
