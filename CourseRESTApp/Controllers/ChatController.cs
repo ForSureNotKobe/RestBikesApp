cd657b8d07985b68ce0da47400303327acddc1fb
Jakub Porębski <j.porebski0@gmail.com>
2021-01-13 12:39:00 +0100

    Improved chat without SignalR

---
+++ CourseRESTApp/Controllers/ChatController.cs
@@ -0,0 +1,97 @@
+﻿using CourseRESTApp.Data;
+using CourseRESTApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using SignalRChat.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CourseRESTApp.Controllers
+{
+    [Authorize]
+    public class ChatController : Controller
+    {
+        private ChatContext _ctx;
+
+        public ChatController(ChatContext ctx) => _ctx = ctx;
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var chats = _ctx.Chats
+                .Include(x => x.Users)
+                .Where(x => !x.Users
+                .Any(y => y.UserId == User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                .ToList();
+
+            return View(chats);
+        }
+
+        [HttpGet]
+        public IActionResult Chatroom(int id)
+        {
+            var chat = _ctx.Chats
+                .Include (x => x.Messages)
+                .FirstOrDefault(x => x.Id == id);
+            return View(chat);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMessage(int chatId, string message)
+        {
+            var Message = new Message
+            {
+                ChatId = chatId,
+                Text = message,
+                Name = User.Identity.Name,
+                TImestamp = DateTime.Now
+            };
+
+            _ctx.Messages.Add(Message);
+            await _ctx.SaveChangesAsync();
+
+            return RedirectToAction("Chatroom", new { id = chatId });
+        }
+
+
+        [HttpPost]        
+        public async Task<IActionResult> Create(string name)
+        {
+            var chat = new Chat
+            {
+                Name = name,
+                Type = ChatType.Room
+            };
+
+            chat.Users.Add(new ChatUser
+            {
+                UserId = User.FindFirst(ClaimTypes.NameIdentifier).Value,
+                Role = UserRole.Admin
+            });
+
+            await _ctx.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> JoinRoom(int id)
+        {
+            var chatUser = new ChatUser {
+                ChatId = id,
+                UserId = User.FindFirst(ClaimTypes.NameIdentifier).Value,
+                Role = UserRole.Member
+            };
+
+            _ctx.ChatUsers.Add(chatUser);
+
+            await _ctx.SaveChangesAsync();
+
+            return RedirectToAction("Chatroom", "Chat", new { id = id });
+        }
+    }
+}
